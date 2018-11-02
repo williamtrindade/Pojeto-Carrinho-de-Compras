e9e043fdf8c766738e8678555151a508208f6a6e
William Trindade <williamtrindade777@gmail.com>
2018-11-02 18:55:20 -0200

    mudar-lang-automaticamente

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,2 @@
-//script
+// muda o atributo lang da página de acordo com o idioma do usuario
+document.querySelector('html').setAttribute('lang', navigator.language)
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +7,7 @@
     <title>iShop</title>
 </head>
 <body>
-    
+
+    <script src="assets/js/index.js"></script>
 </body>
 </html>
